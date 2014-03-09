49e29c69d0cb6daea204f01fc66d96980963d34c
Utsav Anand <utsavanand.aggarwal@gmail.com>
2014-03-10 00:43:00 +0530

    attr_accessible removed from headshot-model

--- app/models/headshot_photo.rb
+++ app/models/headshot_photo.rb
@@ -1,5 +1,5 @@
 class HeadshotPhoto < ActiveRecord::Base
-  attr_accessible :description, :image_content_type, :image_file_name, :image_file_size, :image_updated_at
+  # attr_accessible :description, :image_content_type, :image_file_name, :image_file_size, :image_updated_at
 
   belongs_to :capturable, :polymorphic => true
 
