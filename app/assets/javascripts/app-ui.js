12e846b43594ec3955e520b25a44ab4bef12fa32
Harish.K <harish2704@gmail.com>
2016-03-06 00:27:02 +0530

    UI part

---
+++ app/assets/javascripts/app-ui.js
@@ -0,0 +1,79 @@
+/* globals window, localStorage, FormData, $, Blob, atob, unescape */
+
+
+function ImgApi(){
+  this.apiUrl = 'https://localhost:5667';
+}
+
+ImgApi.prototype = {
+  _requst : function( method, path, body ){
+    body = body || {};
+
+    var args = {
+      url: this.apiUrl + path,
+      method: method,
+    };
+
+    if( method == 'post' ){
+      args.contentType = false;
+      args.processData = false;
+      args.data = new FormData(  );
+      Object.keys( body ).forEach( function( v ){
+        args.data.append( v, body[v] );
+      });
+    }
+
+    return $.ajax( args );
+  },
+
+  dataURItoBlob: function (dataURI) {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+      byteString = atob(dataURI.split(',')[1]);
+    else
+      byteString = unescape(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], {type:mimeString});
+  },
+
+  saveImage: function( dataUrl ){
+    var blob = this.dataURItoBlob( dataUrl );
+    return this._requst( 'post', '/save', { imgData: blob } );
+  },
+
+  updateTweetBtn: function( url ){
+    var elm = $('#btn-tween');
+    var link = 'https://twitter.com/intent/tweet?';
+    var params = $.param({
+      text: "Happy Women's Day",
+      hashtags: 'BecauseSheMatters',
+      url: url,
+    });
+    elm.attr('href', link + params );
+  },
+
+  saveImageAndUpdateUrl: function(){
+
+    var img = localStorage.avatar;
+    var self = this;
+    return this.saveImage( img )
+      .then( function( data ){
+        localStorage.imageUrl = data.imageUrl;
+        self.updateTweetBtn( data.imageUrl );
+        return data;
+      });
+  }
+};
+
+window.imgApi = new ImgApi();
+
