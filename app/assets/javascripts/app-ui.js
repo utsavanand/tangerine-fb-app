b5edbac342ead2478722bf3c48272904aa8be35b
Harish.K <harish2704@gmail.com>
2016-03-06 01:32:16 +0530

    Twitter share

--- app/assets/javascripts/app-ui.js
+++ app/assets/javascripts/app-ui.js
@@ -1,9 +1,50 @@
 /* globals window, localStorage, FormData, $, Blob, atob, unescape */
 
 
+
+
+function dataURItoBlob( dataURI ) {
+  // convert base64/URLEncoded data component to raw binary data held in a string
+  var byteString;
+  if (dataURI.split(',')[0].indexOf('base64') >= 0)
+    byteString = atob(dataURI.split(',')[1]);
+  else
+    byteString = unescape(dataURI.split(',')[1]);
+
+  // separate out the mime component
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+  // write the bytes of the string to a typed array
+  var ia = new Uint8Array(byteString.length);
+  for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+
+  return new Blob([ia], {type:mimeString});
+}
+
+
+
+
+function updateTweetBtn ( url ){
+  var elm = $('#acop-2');
+  var link = 'https://twitter.com/intent/tweet?';
+  var params = $.param({
+    text: "Happy Women's Day",
+    hashtags: 'BecauseSheMatters',
+    url: url,
+  });
+  elm.attr('href', link + params );
+}
+
+
+
+
 function ImgApi(){
+  // this.apiUrl = 'http://www.becauseindiafirstlife.com/img-api/v1'
   this.apiUrl = 'https://localhost:5667';
 }
+
 
 ImgApi.prototype = {
   _requst : function( method, path, body ){
@@ -26,54 +67,34 @@ ImgApi.prototype = {
     return $.ajax( args );
   },
 
-  dataURItoBlob: function (dataURI) {
-    // convert base64/URLEncoded data component to raw binary data held in a string
-    var byteString;
-    if (dataURI.split(',')[0].indexOf('base64') >= 0)
-      byteString = atob(dataURI.split(',')[1]);
-    else
-      byteString = unescape(dataURI.split(',')[1]);
-
-    // separate out the mime component
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-    // write the bytes of the string to a typed array
-    var ia = new Uint8Array(byteString.length);
-    for (var i = 0; i < byteString.length; i++) {
-      ia[i] = byteString.charCodeAt(i);
-    }
-
-    return new Blob([ia], {type:mimeString});
-  },
 
   saveImage: function( dataUrl ){
-    var blob = this.dataURItoBlob( dataUrl );
+    var blob = dataURItoBlob( dataUrl );
     return this._requst( 'post', '/save', { imgData: blob } );
   },
 
-  updateTweetBtn: function( url ){
-    var elm = $('#btn-tween');
-    var link = 'https://twitter.com/intent/tweet?';
-    var params = $.param({
-      text: "Happy Women's Day",
-      hashtags: 'BecauseSheMatters',
-      url: url,
-    });
-    elm.attr('href', link + params );
-  },
-
   saveImageAndUpdateUrl: function(){
-
     var img = localStorage.avatar;
-    var self = this;
+    $('body').addClass('is-uploading');
     return this.saveImage( img )
       .then( function( data ){
+        console.log( data.imageUrl );
         localStorage.imageUrl = data.imageUrl;
-        self.updateTweetBtn( data.imageUrl );
+        updateTweetBtn( data.imageUrl );
+        $('body').removeClass('is-uploading');
         return data;
       });
   }
+
 };
 
+
 window.imgApi = new ImgApi();
+
+
+$(function(){
+  window.imgApi.saveImageAndUpdateUrl();
+});
+
+
 
